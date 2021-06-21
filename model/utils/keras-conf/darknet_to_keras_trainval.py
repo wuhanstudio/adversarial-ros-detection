51227444c4ddd26f524dc5b8f2fdb10c3bbed19d
Wu Han <wuhanstudio@qq.com>
2021-06-21 20:26:47 +0100

    [fix] write flexible class_id

--- model/utils/keras-conf/darknet_to_keras_trainval.py
+++ model/utils/keras-conf/darknet_to_keras_trainval.py
@@ -9,6 +9,7 @@ for file in glob.glob("*.txt"):
     # Using readlines()
     file1 = open(file, 'r')
     data = file1.readlines()[0].split()
+    class_id = int(data[0])
     cx = float(data[1]) * 320
     cy = float(data[2]) * 160
     width = float(data[3]) * 320
@@ -17,5 +18,5 @@ for file in glob.glob("*.txt"):
     ymin = cy - height / 2
     xmax = cx + width / 2
     ymax = cy + height / 2
-    f.write("./IMG/{0}.jpg {1},{2},{3},{4},0\n".format(file.split('.')[0], int(xmin), int(ymin), int(xmax), int(ymax) ))
+    f.write("./IMG/{0}.jpg {1},{2},{3},{4},{5}\n".format(file.split('.')[0], int(xmin), int(ymin), int(xmax), int(ymax), class_id))
 f.close()
